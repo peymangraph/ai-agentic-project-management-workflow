e50fa66494930921e8584ca222612abc114ea2e7
Peyman M Hassan <76390634+peymangraph@users.noreply.github.com>
2026-09-02 14:39:53 -0700

    Scaffold ActionPlanningAgent test

---
+++ phase_1/tests/action_planning_agent.py
@@ -0,0 +1,7 @@
+"""Standalone test for ActionPlanningAgent."""
+
+# TODO: Import ActionPlanningAgent from workflow_agents.base_agents.
+# TODO: Load OPENAI_API_KEY from .env.
+# TODO: Instantiate the agent with the required planning knowledge.
+# TODO: Ask: "One morning I wanted to have scrambled eggs"
+# TODO: Print the extracted list of action steps.
