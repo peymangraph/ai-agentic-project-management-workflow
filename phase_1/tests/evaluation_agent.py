e3397015331de824e27681e870ff062a6b78a4e8
Peyman M Hassan <76390634+peymangraph@users.noreply.github.com>
2026-09-02 14:39:41 -0700

    Scaffold EvaluationAgent test

---
+++ phase_1/tests/evaluation_agent.py
@@ -0,0 +1,7 @@
+"""Standalone test for EvaluationAgent."""
+
+# TODO: Import EvaluationAgent and KnowledgeAugmentedPromptAgent.
+# TODO: Load OPENAI_API_KEY from .env.
+# TODO: Create the worker with the required professor persona and France/London knowledge.
+# TODO: Instantiate EvaluationAgent with max_interactions=10.
+# TODO: Evaluate "What is the capital of France?" and print the returned dictionary.
