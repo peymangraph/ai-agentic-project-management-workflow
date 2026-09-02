7e0fc6814af0e3abed32e7df9412d75fd440c2a7
Peyman M Hassan <76390634+peymangraph@users.noreply.github.com>
2026-09-02 14:39:48 -0700

    Scaffold RoutingAgent test

---
+++ phase_1/tests/routing_agent.py
@@ -0,0 +1,11 @@
+"""Standalone test for RoutingAgent."""
+
+# TODO: Import KnowledgeAugmentedPromptAgent and RoutingAgent.
+# TODO: Load OPENAI_API_KEY from .env.
+# TODO: Create Texas, Europe, and Math knowledge agents.
+# TODO: Assign route dictionaries with descriptions and callable functions.
+# TODO: Test:
+#   "Tell me about the history of Rome, Texas"
+#   "Tell me about the history of Rome, Italy"
+#   "One story takes 2 days, and there are 20 stories"
+# TODO: Print each routing result.
