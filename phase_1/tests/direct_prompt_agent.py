10acf7eae71a093d49f0e6a887a3921b32092602
Peyman M Hassan <76390634+peymangraph@users.noreply.github.com>
2026-09-02 14:39:11 -0700

    Scaffold DirectPromptAgent test

---
+++ phase_1/tests/direct_prompt_agent.py
@@ -0,0 +1,8 @@
+"""Standalone test for DirectPromptAgent."""
+
+# TODO: Import DirectPromptAgent from workflow_agents.base_agents.
+# TODO: Load OPENAI_API_KEY from .env.
+# TODO: Instantiate DirectPromptAgent.
+# TODO: Ask: "What is the Capital of France?"
+# TODO: Print the response and explain that the knowledge source is the LLM's
+# general pretrained knowledge.
